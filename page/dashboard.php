f70fc77337f2404c4dad0e3cc2add7e4fa45d6a2
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-01-29 22:20:05 +0700

    GluonTMS

--- page/dashboard.php
+++ page/dashboard.php
@@ -27,7 +27,7 @@ $priceList = json_decode(file_get_contents($priceListJsonPath), true);
     <link rel="stylesheet" href="../public/stylesheets/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <link rel="shortcut icon" href="../assets/images/20250127_182602.png" type="image/x-icon">
+    <link rel="website icon" href="../assets/images/gluonLogo.png" type="png">
 
     <title>Gluon: Task Management System</title>
 </head>
